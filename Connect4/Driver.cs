ffe5f0cfe0ce0f88605b2c558a189b969a700c7a
Timothy Zink <timdzink@gmail.com>
2023-11-04 10:04:11 -0600

    convert Connect4 project to library, start structuring

---
+++ Connect4/Driver.cs
@@ -0,0 +1,14 @@
+﻿namespace Connect4
+{
+    public static class Driver
+    {
+        // driver code
+        public static void Test()
+        {
+            GameState gameState = new GameState();
+            gameState.drop(1, 0);
+            gameState.drop(1, 0);
+            gameState.drop(1, 0);
+        }
+    }
+}
