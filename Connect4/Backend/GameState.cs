7b163498ce574e242d27884a4403d8a475f8f0c1
acarston <amcarston@gmail.com>
2023-11-03 23:29:25 -0600

    port to VS

--- Connect4/Backend/GameState.cs
+++ Connect4/Backend/GameState.cs
@@ -1,7 +1,7 @@
-class GameState {
-    private const int BOARD_HEIGHT = 6, BOARD_WIDTH = 7;
-
-    static void Main(string[] args) {
-        return;
+﻿namespace Connect4.Backend
+{
+    internal class GameState
+    {
+        private const int BOARD_HEIGHT = 6, BOARD_WIDTH = 7;
     }
 }
