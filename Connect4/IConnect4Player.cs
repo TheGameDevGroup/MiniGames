73ec1dba9427c91b86ec0265a5ee55c6ff605cc8
Timothy Zink <timdzink@gmail.com>
2023-11-04 11:35:25 -0600

    add basic code for handling connect4 game

---
+++ Connect4/IConnect4Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Connect4
+{
+	/// <summary>
+	/// This is used to enforce common behavior across different implementations.
+	/// 
+	/// If you want to make a Connect 4 (AI) player, implement this interface.
+	/// </summary>
+	public interface IConnect4Player
+	{
+		/// <summary>
+		/// Choose a move to play.
+		/// </summary>
+		/// <param name="gameState">An array representing the current game state. (rows then columns).</param>
+		/// <param name="playerToken">The int that represents this player's pieces in the game.</param>
+		/// <returns>A column number to drop a piece into.</returns>
+		int MakeMove(in int[,] gameState, int playerToken);
+	}
+}
---
+++ Connect4/TestPlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace Connect4
+{
+    internal class TestPlayer : IConnect4Player
+    {
+        public int MakeMove(in int[,] gameState, int playerToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
