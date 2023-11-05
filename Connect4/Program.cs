0b3c8cfe2e59d7b4b56125a691d2004115431bd0
acarston <amcarston@gmail.com>
2023-11-04 21:29:22 -0600

    add more driver code

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -45,5 +45,25 @@ internal class Program
         gameState.Drop(1, 1);
         gameState.Drop(2, 1);
         gameState.Drop(3, 1);
+
+        gameState.Drop(1, 1);
+        gameState.Drop(2, 1);
+        gameState.Drop(3, 1);
+        gameState.Drop(2, 1);
+        gameState.Drop(3, 1);
+        gameState.Drop(3, 1);
+
+        gameState = new GameState();
+        gameState.Drop(0, 1);
+        gameState.Drop(1, 1);
+        gameState.Drop(2, 1);
+        gameState.Drop(3, 1);
+
+        gameState.Drop(0, 1);
+        gameState.Drop(1, 1);
+        gameState.Drop(2, 1);
+        gameState.Drop(1, 1);
+        gameState.Drop(0, 1);
+        gameState.Drop(0, 1);
     }
 }
