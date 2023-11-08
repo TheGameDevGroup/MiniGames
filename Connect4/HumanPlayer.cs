2dd3f1df5602819b57c7abae1a916b155f4a164b
Timothy Zink <timdzink@gmail.com>
2023-11-08 09:47:56 -0700

    Finish human player

---
+++ Connect4/HumanPlayer.cs
@@ -0,0 +1,31 @@
+﻿namespace Connect4
+{
+	public class HumanPlayer : IConnect4Player
+	{
+
+		public event EventHandler<int[,]> UpdateUI;
+		private ManualResetEventSlim MoveSubmitEvent = new(false);
+		public int ProposedMove;
+		public HumanPlayer(EventHandler<int[,]> updateUICallback)
+		{
+			UpdateUI = updateUICallback;
+		}
+		public int MakeMove(in int[,] gameState, int playerToken)
+		{
+			MoveSubmitEvent.Reset();
+			MoveSubmitEvent.Wait();
+			lock (this)
+			{
+				return ProposedMove;
+			}
+		}
+		public void SubmitMove(int column)
+		{
+			lock (this)
+			{
+				ProposedMove = column;
+				MoveSubmitEvent.Set();
+			}
+		}
+	}
+}
