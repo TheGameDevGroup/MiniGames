f67026a7b8255db204effcd7558f972d91c30c44
Timothy Zink <timdzink@gmail.com>
2023-11-08 16:35:57 -0700

    highlight win

--- UI/Connect4Components/TokenSpace.cs
+++ UI/Connect4Components/TokenSpace.cs
@@ -4,65 +4,68 @@ using UI.Properties;
 
 namespace UI
 {
-    public partial class TokenSpace : UserControl
-    {
-        public int ColumnIndex { get; init; }
-        public int RowIndex { get; init; }
-        private ImageAttributes Attributes { get; set; } = new();
-        private Image Image { get; set; } = new Bitmap(Resources.test);
+	public partial class TokenSpace : UserControl
+	{
+		public int ColumnIndex { get; init; }
+		public int RowIndex { get; init; }
+		private ImageAttributes Attributes { get; set; } = new();
+		private Image Image { get; set; } = new Bitmap(Resources.test);
 
-        public TokenSpace(int row, int column, Color startColor)
-        {
-            InitializeComponent();
-            RowIndex = row;
-            ColumnIndex = column;
-            pictureBox1.Image = Image;
-            SetToken(startColor);
-            pictureBox1.Paint += (object? sender, PaintEventArgs e) =>
-            {
-                e.Graphics.DrawImage(
-                    Image,
-                    new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height),
-                    0, 0,
-                    Image.Width, Image.Height,
-                    GraphicsUnit.Pixel,
-                    Attributes);
-            };
-        }
-        public void SetToken(Color color)
-        {
-            ColorMatrix matrix = new(new float[5][]
-            {
-                new float[5] { color.R / 255f, 0, 0, 0, 0},
-                new float[5] { 0, color.G / 255f, 0, 0, 0},
-                new float[5] { 0, 0, color.B / 255f, 0, 0},
-                new float[5] { 0, 0, 0, color.A / 255f, 0},
-                new float[5] { 0, 0, 0, 0, 1}
-            });
-            Attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            if (pictureBox1.InvokeRequired)
-            {
-                pictureBox1.Invoke(pictureBox1.Refresh);
-            }
-            else
-            {
-                pictureBox1.Refresh();
-            }
-        }
+		public TokenSpace(int row, int column, Color startColor)
+		{
+			InitializeComponent();
+			RowIndex = row;
+			ColumnIndex = column;
+			pictureBox1.Image = Image;
+			SetToken(startColor);
+			pictureBox1.Paint += (object? sender, PaintEventArgs e) =>
+			{
+				e.Graphics.DrawImage(
+					Image,
+					new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height),
+					0, 0,
+					Image.Width, Image.Height,
+					GraphicsUnit.Pixel,
+					Attributes);
+			};
+		}
+		public void SetToken(Color color)
+		{
+			ColorMatrix matrix = new(new float[5][]
+			{
+				new float[5] { color.R / 255f, 0, 0, 0, 0},
+				new float[5] { 0, color.G / 255f, 0, 0, 0},
+				new float[5] { 0, 0, color.B / 255f, 0, 0},
+				new float[5] { 0, 0, 0, color.A / 255f, 0},
+				new float[5] { 0, 0, 0, 0, 1}
+			});
+			Attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+			if (pictureBox1.InvokeRequired)
+			{
+				pictureBox1.Invoke(pictureBox1.Refresh);
+			}
+			else
+			{
+				pictureBox1.Refresh();
+			}
+		}
+		public void SetBackGround(Color color)
+		{
+			pictureBox1.BackColor = color;
+		}
+		private void Picture_MouseUp(object sender, MouseEventArgs e)
+		{
+			this.OnMouseUp(e);
+		}
 
-        private void Picture_MouseUp(object sender, MouseEventArgs e)
-        {
-            this.OnMouseUp(e);
-        }
+		private void Picture_MouseDown(object sender, MouseEventArgs e)
+		{
+			this.OnMouseDown(e);
+		}
 
-        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-        {
-            this.OnMouseDown(e);
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            this.OnClick(e);
-        }
-    }
+		private void Picture_Click(object sender, EventArgs e)
+		{
+			this.OnClick(e);
+		}
+	}
 }
